1694e77f100e77828ffb6ad5afa9f57051832e8b
Daiana <dairotchen@gmail.com>
2026-07-21 20:19:08 +0200

    feat(model): add Villian class

---
+++ src/main/java/com/swingy/model/Villian.java
@@ -0,0 +1,20 @@
+package com.swingy.model;
+
+public class Villian {
+    private int level;
+    private int attack;
+    private int defense;
+    private int hitPoints;
+
+    public Villian(int level, int attack, int defense, int hitPoints) {
+        this.level = level;
+        this.attack = attack;
+        this.defense = defense;
+        this.hitPoints = hitPoints;
+    }
+
+    public int getLevel()    { return level; }
+    public int getAttack()   { return attack; }
+    public int getDefense()  { return defense; }
+    public int getHitPoints(){ return hitPoints; }
+}
