6217dcc611b05ed36ae8960cc40590e73ed03a8f
Daiana <dairotchen@gmail.com>
2026-08-14 10:22:42 +0200

    feat(view): add GameView interface for game interaction methods

---
+++ src/main/java/com/swingy/view/GameView.java
@@ -0,0 +1,43 @@
+package com.swingy.view;
+
+import java.util.List;
+
+import com.swingy.model.DirectionType;
+import com.swingy.model.artifact.Artifact;
+import com.swingy.model.battle.BattleResult;
+import com.swingy.model.hero.Hero;
+import com.swingy.model.hero.HeroClass;
+import com.swingy.model.map.GameMap;
+
+// because GuiView might need to inherit from 2 diferent classes, we will use an
+// interface instead of a class
+public interface GameView {
+    boolean askNewHero();
+
+    String askHeroName();
+
+    HeroClass askHeroClass();
+
+    Hero askSelectHero(List<Hero> heroes);
+
+    void showHeroDetails(Hero hero);
+
+    DirectionType askDirection();
+
+    boolean askFight();
+
+    boolean askArtifactPickup(Artifact artifactDetails);
+
+    void drawMap(GameMap map, Hero hero);
+
+    void showMessage(String message);
+
+    void showBattleResult(BattleResult result);
+
+    void showGameOver(String message);
+
+    void showVictory(String message);
+
+    boolean askPlayAgain();
+
+}
