b3a7e080f5e0695b14a6a7f29e2d54528b739d24
Daiana <dairotchen@gmail.com>
2026-08-18 17:22:42 +0200

    feat(Main): implement main method to initialize game with console view

--- src/main/java/com/swingy/Main.java
+++ src/main/java/com/swingy/Main.java
@@ -1,5 +1,47 @@
 package com.swingy;
 
+import com.swingy.model.map.GameMap;
+import com.swingy.model.villain.Villian;
+import com.swingy.repository.HeroRepository;
+import com.swingy.view.GameView;
+import com.swingy.model.hero.Hero;
+import com.swingy.view.console.ConsoleView;
+import com.swingy.controller.Controller;
+import com.swingy.repository.FileHeroRepository;
+
 public class Main {
-      public static void main(String[] args)   {}
+
+      public static void main(String[] args) {
+            GameView view;
+            HeroRepository repository = new FileHeroRepository();
+            Controller controller;
+
+            if (args.length < 1) {
+                  System.out.println("Please specify the view type: console or gui");
+                  return;
+            }
+            if (args.length > 1) {
+                  System.out.println("Too many arguments. Please specify only the view type: console or gui");
+                  return;
+            } else {
+                  args[0] = args[0].toLowerCase();
+            }
+            if (!args[0].equals("console") && !args[0].equals("gui")) {
+                  System.out.println("Invalid input. Please specify 'console' or 'gui'");
+                  return;
+            }
+
+            if (args[0].equals("console")) {
+                  System.out.println("Starting in console mode...");
+                  view = new ConsoleView(); // or new GuiView() for GUI
+
+                  // } else {
+                  // System.out.println("Starting in GUI mode...");
+                  // view = new GuiView(); // Implement this class for GUI
+                  // }
+                  controller = new Controller(view, repository);
+                  controller.startGame(); // Start a new game);
+            }
+            // scanner.close();/// where to close?
+      }
 }
