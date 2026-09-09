a23b43dbad0b022055288198771fe1a4365dba63
Daiana <dairotchen@gmail.com>
2026-09-09 21:39:38 +0200

    fix (HeroStats DTO) add artiffacts

--- src/main/java/com/swingy/view/HeroStats.java
+++ src/main/java/com/swingy/view/HeroStats.java
@@ -1,4 +1,4 @@
 package com.swingy.view;
 
-public record HeroStats(String name, String type, int level, int xp, int attack, int defense, int hitPoints) {
+public record HeroStats(String name, String type, int level, int xp, int attack, int defense, int hitPoints, String weapon, String armor, String helm) {
 }
